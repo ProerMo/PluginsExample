941349bdfca9b256b62551a101a2bce0f8a0b604
莫鹏飞 <mopengfei@listener.com.cn~>
2020-08-09 16:37:17 +0800

    init

---
+++ module_plugins/src/main/groovy/com/proermo/MyPlugin.groovy
@@ -0,0 +1,16 @@
+package com.proermo
+
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+public class MyPlugin implements Plugin<Project> {
+
+
+    @Override
+    void apply(Project project) {
+        println "========================"
+        println "hello world!"
+        println "========================"
+    }
+}
